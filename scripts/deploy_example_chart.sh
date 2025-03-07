63aef2a4e6b93011514ff926eec971b23629bdc4
Josef Hak <jhak@mirantis.com>
2025-03-07 18:44:32 +0100

    Add helm dependency update

--- scripts/deploy_example_chart.sh
+++ scripts/deploy_example_chart.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 chart=apps/$APP/example
+helm dependency update $chart
 helm dependency build $chart
 KUBECONFIG=kcfg helm upgrade --install $APP $chart -n $APP --create-namespace
 
