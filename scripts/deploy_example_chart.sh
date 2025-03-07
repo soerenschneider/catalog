367f421e7b0748e11b75611d6a81378f8abc383b
Josef Hak <jhak@mirantis.com>
2025-03-07 18:50:03 +0100

    Revert "Add helm dependency update"
    This reverts commit 6d40f4dc9e5d56a0a4cef7149b98e9f163836f86.

--- scripts/deploy_example_chart.sh
+++ scripts/deploy_example_chart.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 chart=apps/$APP/example
-helm dependency update $chart
 helm dependency build $chart
 KUBECONFIG="kcfg_$TEST_MODE" helm upgrade --install $APP $chart -n $APP --create-namespace
 
