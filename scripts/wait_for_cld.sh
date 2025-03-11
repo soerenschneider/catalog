bcb46162b7e150e72fdb05b90c79398a6892c639
Josef Hak <jhak@mirantis.com>
2025-03-11 12:55:06 +0100

    Use kcm-system namespace in wait_for_cld.sh script

--- scripts/wait_for_cld.sh
+++ scripts/wait_for_cld.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 while true; do
-    cld_out=$(kubectl get cld | grep "$CLDNAME")
+    cld_out=$(kubectl get cld -n kcm-system | grep "$CLDNAME")
     if echo "$cld_out" | grep 'ClusterDeployment is ready'; then
         break
     fi
-    echo "cld_out"
+    echo "$cld_out"
     echo "Waiting for cluster..."
     sleep 3
 done
