ac5d5ee62d916c7d35b595416b599bfbf38213e0
Josef Hak <jhak@mirantis.com>
2025-03-11 12:37:05 +0100

    Use ./scripts/wait_for_deployment.sh in ./scripts/deploy_k0rdent.sh

--- scripts/deploy_k0rdent.sh
+++ scripts/deploy_k0rdent.sh
@@ -5,6 +5,8 @@ if kind get clusters | grep "k0rdent"; then
     echo "k0rdent kind cluster already exists"
 else
     kind create cluster -n k0rdent
+    kind get kubeconfig -n k0rdent > "kcfg_k0rdent"
+    chmod 0600 "kcfg_k0rdent" # set minimum attributes to kubeconfig (owner read/write)
 fi
 
 if helm get notes kcm -n kcm-system; then
@@ -12,3 +14,6 @@ if helm get notes kcm -n kcm-system; then
 else
     helm install kcm oci://ghcr.io/k0rdent/kcm/charts/kcm --version 0.1.0 -n kcm-system --create-namespace
 fi
+
+TEST_MODE=k0rdent NAMESPACE=kcm-system ./scripts/wait_for_deployment.sh
+TEST_MODE=k0rdent NAMESPACE=projectsveltos ./scripts/wait_for_deployment.sh
